2db684abf3f8daf54df0515a6814f6721ed4678c
Michael E. Karpeles (Mek) <michael.karpeles@gmail.com>
2022-12-08 00:09:14 +0000

    fixing type errors

--- openlibrary/coverstore/code.py
+++ openlibrary/coverstore/code.py
@@ -287,16 +287,18 @@ class cover:
         if (size == "L" or size == "") and self.is_cover_in_cluster(value):
             url = zipview_url_from_id(int(value), size)
             raise web.found(url)
-        # These items have been tar archived in items, starting with covers_0008
-        if 8010000 > int(value) >= 8000000:
-            prefix = f"{size.lower()}_" if size else ""
-            pid = "%010d" % int(value)
-            item_id = f"{prefix}covers_{pid[:4]}"
-            item_tar = f"{prefix}covers_{pid[:4]}_{pid[4:6]}.tar"
-            item_file = f"{pid}{'-' + size.upper() if size else ''}"
-            path = f"{item_id}/{item_tar}/{item_file}.jpg"
-            protocol = web.ctx.protocol
-            raise web.found(f"{protocol}://archive.org/download/{path}")
+
+        # These covers are tar archived in items, starting with covers_0008
+        if isinstance(value, int) or value.isnumeric():
+            if (8010000 > int(value) >= 8000000):
+                prefix = f"{size.lower()}_" if size else ""
+                pid = "%010d" % int(value)
+                item_id = f"{prefix}covers_{pid[:4]}"
+                item_tar = f"{prefix}covers_{pid[:4]}_{pid[4:6]}.tar"
+                item_file = f"{pid}{'-' + size.upper() if size else ''}"
+                path = f"{item_id}/{item_tar}/{item_file}.jpg"
+                protocol = web.ctx.protocol
+                raise web.found(f"{protocol}://archive.org/download/{path}")
 
         d = self.get_details(value, size.lower())
         if not d:
